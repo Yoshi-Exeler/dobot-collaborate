fbf5bfe20adf87c76a20e887329b265fc24c4240
Yoshi Exeler <yoshi.exeler@intellitrend.de>
2022-04-26 13:19:58 +0200

    [ADD] add example using two dobots

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-from wrapper.wrapper import DobotWrapper
+from wrapper.wrapper import DobotWrapper, Position
 import platform
 
-print(platform.architecture())
+DOBOT_ONE_PORT = "COM3"
+DOBOT_TWO_PORT = "COM5"
 
-robot = DobotWrapper("COM3")
-ok = robot.connect()
-print(ok)
+robot_one = DobotWrapper(DOBOT_ONE_PORT)
+robot_two = DobotWrapper(DOBOT_TWO_PORT)
+
+if not (robot_one.connect()):
+    print("connection with dobot one failed")
+    exit(1337)
+
+
+if not (robot_two.connect()):
+    print("connection with dobot two failed")
+    exit(1337)
+
+print("both dobots are homed and ready")
+
+# robot_one.move(Position(0,0,0,0)), moves the robot to the specifiec X,Y,Z coordinated and applies the specified Rotation
+
+# robot_one.getPosition(), returns the current position of the dobot
+
+# await robot_one.awaitMotionFinish() will block until the robot has finished its current movement
