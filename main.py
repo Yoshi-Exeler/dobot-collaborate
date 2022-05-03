15c384ee9226f66fdeae7d30952bcc9623b47d6e
Yoshi Exeler <yoshi.exeler@intellitrend.de>
2022-05-03 09:11:23 +0200

    [ADD] add fixed example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from wrapper.wrapper import DobotWrapper, Position
-import platform
+import asyncio
 
 DOBOT_ONE_PORT = "COM3"
 DOBOT_TWO_PORT = "COM5"
@@ -18,8 +18,30 @@ if not (robot_two.connect()):
 
 print("both dobots are homed and ready")
 
-# robot_one.move(Position(0,0,0,0)), moves the robot to the specifiec X,Y,Z coordinated and applies the specified Rotation
+# robot_one.move(Position(0,0,0,0)), moves the robot to the specified X,Y,Z coordinated and applies the specified Rotation, the command is queued but the method does not block
+
+# await robot_one.move(Position(0,0,0,0)), moves the robot to the specified Position and blocks until the motion completes
 
 # robot_one.getPosition(), returns the current position of the dobot
 
-# await robot_one.awaitMotionFinish() will block until the robot has finished its current movement
+# robot_one.awaitMotionFinish() will block until the robot has finished its current movement
+
+
+
+# now lets do some practical examples
+async def main():
+    # first we will run two movements in sequence
+    await robot_one.move(Position(0,0,0,0))
+    await robot_two.move(Position(0,0,0,0))
+
+    # now sleep for 3s
+    asyncio.sleep(3000)
+
+    # finally, we will run two movements at the same time
+    robot_one.move(Position(30,30,30,0))
+    robot_two.move(Position(-30,-30,-30,0))
+
+    # now sleep for 3s
+    asyncio.sleep(3000)
+
+main()
