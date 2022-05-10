57213c7a1c2fe8dd39b385e74311be5c136e31cb
Yoshi Exeler <yoshi.exeler@intellitrend.de>
2022-05-10 10:18:56 +0200

    [FIX] fix rendering

--- game/game.py
+++ game/game.py
@@ -1,11 +1,78 @@
 from time import sleep
 from typing import List
 import asyncio
-from render.render import Renderer
+from wrapper.wrapper import DobotWrapper, Position
+from game.game import *
 
 PLAYER_ONE_SYMBOL = 'O'
 PLAYER_TWO_SYMBOL = "X"
 DEBUG = True
+
+offsets = []
+offsets.append(Position(-50, 50, 0, 0))
+offsets.append(Position(0, 50, 0, 0))
+offsets.append(Position(50, 50, 0, 0))
+offsets.append(Position(-50, 0, 0, 0))
+offsets.append(Position(0, 0, 0, 0))
+offsets.append(Position(50, 0, 0, 0))
+offsets.append(Position(-50, -50, 0, 0))
+offsets.append(Position(0, -50, 0, 0))
+offsets.append(Position(50, -50, 0, 0))
+
+
+class Renderer:
+    __renderState: List[str]
+    __robotOne: DobotWrapper  # the first player's dobot
+    __robotTwo: DobotWrapper  # the second player's dobot
+    __toggle: bool  # switch dobots after every move made
+
+    def __init__(self, robotOne: DobotWrapper, robotTwo: DobotWrapper):
+        self.__robotOne = robotOne
+        self.__robotTwo = robotTwo
+        self.__toggle = False
+        self.__renderState = List[str]
+        pass
+
+    def render(self, board: List[str]):
+        # first we print the state to stdout
+        print(board[0], board[1], board[2])
+        print(board[3], board[4], board[5])
+        print(board[6], board[7], board[8])
+        # get the cell where the last move was made
+        deltaCell = self.findDeltaCell(board)
+        # get the symbol that was last placed
+        symbolPlaced = board[deltaCell]
+        # place this symbol on the paper using the current player's dobot
+        self.__placeSymbol(deltaCell, symbolPlaced)
+        # update the render state
+        self.__renderState = board
+        # flip the toggle so the other dodot will be used next
+        self.__toggle = not self.__toggle
+
+    def findDeltaCell(self, board: List[str]) -> int:
+        for i in range(len(board)):
+            if not board[i] == self.__renderState[i]:
+                return i
+        return -1
+
+    async def placeSymbol(self, index: int, symbol: str):
+        # get our dobot instance
+        robot = self.__robotOne
+        if self.__toggle:
+            robot = self.__robotTwo
+        # get the offset for the target field
+        offset = offsets[index]
+        # get our current position
+        pos = robot.getPosition()
+        # compute our target position
+        target = Position(pos.X+offset.X, pos.Y+offset.Y,
+                          pos.Z+offset.Z, pos.R+offset.R)
+        # move to the target position
+        await robot.move(target)
+        # draw the symbol
+        # move back to the starting position
+        await robot.move(pos)
+
 
 
 class TicTacToe:
@@ -45,3 +112,4 @@ class TicTacToe:
         # sould render a text based representation of the board to the standard output.
         # This should be called after every placeSymbol call when DEBUG is set to true.
         pass
+
--- main.py
+++ main.py
@@ -1,13 +1,37 @@
+from telnetlib import COM_PORT_OPTION
 from time import sleep
-from game.game import TicTacToe
+from game.game import Renderer, TicTacToe
 from ai.ai import AIPlayer
 from human.human import HumanPlayer
 import asyncio
 
+from wrapper.wrapper import DobotWrapper
+
+print("[Init] connecting to dobots")
+
+robotOne = DobotWrapper("COM3",False,False)
+
+robotTwo = DobotWrapper("COM3",True,True)
+
+
+if not (robotOne.connect()):
+    print("[Init] connection with dobot one failed")
+    exit(1337)
+
+
+if not (robotTwo.connect()):
+    print("[Init] connection with dobot two failed")
+    exit(1337)
+
+print("[Init] both dobots are homed and ready")
+
 print("[Init] creating game instance")
 
+# create our renderer instance
+renderer = Renderer(robotOne,robotTwo)
+
 # Create a game instance to be used
-gameInstance = TicTacToe()
+gameInstance = TicTacToe(renderer)
 
 print("[Init] launching player coroutines")
 
